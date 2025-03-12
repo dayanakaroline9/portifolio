47599837b9150bc410c88d6a8e800ead030a68aa
Dayana Lima <dayanakaroline9@gmail.com>
2025-03-12 00:54:15 -0300

    site criado em vue.js com quasar framework para melhorar a qualidade do site

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+Layout Builder
+Visible Header
+Visible Footer
+Visible left-side Drawer
+Visible right-side Drawer
+Inject Drawer content for scrolling
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar align="center">
+        <q-toolbar-title>
+            <img src="../assets/logoescuro.png" style="">
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="center">
+        <q-route-tab to="/page1" label="Home" />
+        <q-route-tab to="/page2" label="Sobre Mim" />
+        <q-route-tab to="/page3" label="Projetos" />
+        <q-route-tab to="/page4" label="Serviços" />
+        <q-route-tab to="/page5" label="Contato" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
